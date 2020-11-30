43ea648b35780d7a18afc1d1df455db46f9e20aa
Devaansh2007 <74101745+Devaansh2007@users.noreply.github.com>
2020-11-30 18:46:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,34 @@
-var fixedRect, movingRect;
-
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+ fixedRect=createSprite(400, 100, 50, 80);  
+ fixedRect.shapeColor="green"; 
+ movingRect=createSprite(400,800,80,30);
+ movingRect.shapeColor="green"; 
+ gameObject1 = createSprite(100,200,80,30); 
+  gameObject1.shapeColor = "green";
+   gameObject2 = createSprite(200,200,80,30); 
+   gameObject2.shapeColor = "green";
+   gameObject3 = createSprite(300,200,80,30);
+   gameObject3.shapeColor = "green"; 
+   gameObject4 = createSprite(400,200,80,30);
+   gameObject4.shapeColor = "green";
+   movingRect.velocityY = -5;
+   fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(0,0,0);  
-
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
-    movingRect.velocityX = movingRect.velocityX * (-1);
-    fixedRect.velocityX = fixedRect.velocityX * (-1);
-  }
-  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-    movingRect.velocityY = movingRect.velocityY * (-1);
-    fixedRect.velocityY = fixedRect.velocityY * (-1);
-  }
+  background(0);
+  bounceOff(movingRect,fixedRect)
+  
+  // movingRect.x=World.mouseX;
+  //  movingRect.y=World.mouseY;
+  //  if(isTouching(movingRect,gameObject1)){  
+   //    movingRect.shapeColor="red"; 
+  //     gameObject1.shapeColor="red"; 
+    //  }  
+      //else
+   //   { movingRect.shapeColor="green";
+   //gameObject1.shapeColor="green"; }
+   
   drawSprites();
-}
+    }
